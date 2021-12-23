90cccde33e5bf9665dd7251ce857023c8e6c5af1
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-23 11:37:17 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,7 +1,4 @@
-import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
-import { AccountModel } from '../../../domain/models/account'
-import { Encrypter } from '../../protocols/encrypter'
-import { Promise } from 'mongoose'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter
@@ -11,6 +8,6 @@ export class DbAddAccount implements AddAccount {
 
   async add (account: AddAccountModel): Promise<AccountModel> {
     this.encrypter.encrypt(account.password)
-    return new Promise(resolve => resolve(null))
+    return await new Promise(resolve => resolve(null))
   }
 }
