1dfe79409499b5f52d6a985a58171f3115de5a71
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-29 09:57:37 -0300

    refactor: improve file names

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, HttpResponse, Controller, Validation } from './signup-protocols'
+import { HttpRequest, HttpResponse, Controller, Validation } from './signup-protocols-controller'
 import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 import { AddAccount } from '../../../domain/usecases/add-account'
 
