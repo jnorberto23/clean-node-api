73f9a09ad037ef1b0da006971314f37a35a0f8fa
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-29 10:25:28 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.mongoUri || 'mongodb://127.0.0.1:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.jwtSecret || 'soakdo123919951!!sjkio'
 }
