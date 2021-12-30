75a40ef40fdb43787aa3b2e27c5b9b654c5a1a7e
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-30 14:59:14 -0300

    feat: ensure add survey route returns 403 if no access token is provided

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -2,7 +2,7 @@ import { Controller } from '../../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
 import { AddSurveyController } from '../../../../../presentation/controllers/survey/add-survey/add-survey-controller'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
-import { makeDbAddSurvey } from '../../../use-cases/account/survey/add-survey/db-add-survey-factory'
+import { makeDbAddSurvey } from '../../../use-cases/survey/add-survey/db-add-survey-factory'
 
 export const makeAddSurveyController = (): Controller => {
   const controller = new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
