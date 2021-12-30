dd7443fc70b3ed8a36b7c22c688fcc875f0b4240
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-30 14:05:56 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jtw-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jtw-adapter/jwt-adapter.spec.ts
@@ -47,5 +47,13 @@ describe('JWT Adapter', () => {
       const value = await sut.decrypt('any_token')
       expect(value).toBe('any_token')
     })
+    test('should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
