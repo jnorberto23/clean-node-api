6c4775c2e3b4c6e2b7d135865032f616a3ce28b6
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-30 17:57:03 -0300

    test: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -56,4 +56,9 @@ describe('DbLoadSurveys', () => {
     await sut.load()
     expect(loadAllSpy).toHaveBeenCalled()
   })
+  test('should return a list of surveys on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
 })
