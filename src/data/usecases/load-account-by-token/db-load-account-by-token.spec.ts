55d634d74c262f04f0b536f7848c93c2c8770be7
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-30 12:40:33 -0300

    test: ensure DbLoadAccountByToken returns an account on success

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -68,4 +68,9 @@ describe('DbLoadAccountByToken Usecase', () => {
     const account = await sut.load('any_token', 'any_role')
     expect(account).toBe(null)
   })
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.load('any_token', 'any_role')
+    expect(account).toEqual(makeFakeAccount())
+  })
 })
