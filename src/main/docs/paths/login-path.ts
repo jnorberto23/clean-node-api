c906cb4ff72fd72f8dac7d415e4f3a200eacb1b8
jnorberto23 <joaovictor.norberto@outlook.com>
2022-01-05 10:13:01 -0300

    docs: add login api docs

---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,29 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API para autenticar usuário',
+    description: 'Essa rota pode ser executada por **qualquer usuário**',
+    requestBody: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
